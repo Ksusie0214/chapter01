58dc797a9ff8fa565ca5ced48df09ccee3c733af
Ksusie0214 <tdlrtmvldj@gmail.com>
2023-12-28 14:53:50 +0900

    While문 break;

---
+++ src/com/javaex/ex04/Ex10.java
@@ -0,0 +1,20 @@
+package com.javaex.ex04;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		int i = 1;
+		
+		
+		
+		while(true) {
+			i++;
+			if(i%6==0 && i%14==0) {
+			System.out.println(i); break;
+			}
+			
+		}
+		
+		
+	}
+
+}
